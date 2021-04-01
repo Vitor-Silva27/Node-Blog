776f688cdfb3a5002c6d7c7d8efcb661a89c87fa
Vitor Silva <developer.vitorsilva@gmail.com>
2021-04-01 14:45:49 -0300

    created view article

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const connection = require('./database/database');
+const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/articlesController");
@@ -13,31 +13,54 @@ const Category = require("./categories/Category");
 app.set("view engine", "ejs");
 
 //static
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 //database
 connection
-    .authenticate()
-    .then(() => {
-        console.log("Database connection was a success")
-    }).catch((err) => {
-        console.log(err);
-    })
-
+  .authenticate()
+  .then(() => {
+    console.log("Database connection was a success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use("/", categoriesController);
 app.use("/", articlesController);
 
-app.get("/", (req, res) => {
-    Article.findAll().then(articles => {
-        res.render('index.ejs', {articles: articles});     
+app.get("/", (_, res) => {
+  Article.findAll().then((articles) => {
+    res.render("index.ejs", { articles: articles });
+  });
+});
+
+app.get("/:slug", (req, res) => {
+  const slug = req.params.slug;
+  articlesController
+    .findOne({
+      where: {
+        slug: slug,
+      },
     })
+    .then((article) => {
+      if (article != undefined) {
+        res.render("");
+      } else {
+        res.redirect("/");
+      }
+    })
+    .catch((err) => {
+      res.redirect("/");
+      console.log(err);
+    });
 });
 
 app.listen(8080, () => console.log("Is running!"));
