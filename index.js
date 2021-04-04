a857e97a335c737a84c98e7587d4a8cfdee8d4e4
Vitor Silva <developer.vitorsilva@gmail.com>
2021-04-03 22:19:19 -0300

    added express-session

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const session = require('express-session');
 const connection = require("./database/database");
+
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/articlesController");
@@ -13,6 +15,14 @@ const Category = require("./categories/Category");
 const User = require('./users/User');
 //view engine
 app.set("view engine", "ejs");
+
+// Sessions
+app.use(session({
+  secret: "open-sesame",
+  cookie: { maxAge: 300000 }
+}));
+
+
 
 //static
 app.use(express.static("public"));
@@ -38,7 +48,6 @@ connection
 app.use("/", categoriesController);
 app.use("/", articlesController);
 app.use("/", usersController);
-
 
 app.get("/", (_, res) => {
   Article.findAll({
--- users/UsersController.js
+++ users/UsersController.js
@@ -1,35 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('./User');
-const bcrypt = require('bcryptjs');
+const User = require("./User");
+const bcrypt = require("bcryptjs");
 
-
-router.get('/admin/users', (req, res) =>{
-    res.send("listagem de usuarios");
-});
-
-router.get('/admin/users/create', (req, res) => {
-    res.render('admin/users/create');
-});
-
-router.post("/users/create", (req, res) =>{
-    const email = req.body.email;
-    const password = req.body.password;
-
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-
-    User.create({
-        email:email,
-        password: hash
-    }).then(() => {
-        res.redirect('/');
-    }).catch(err =>{
-        res.redirect("/");
+router.get("/admin/users", (req, res) => {
+    User.findAll().then(users => {
+        res.render('admin/users/index',{users: users});
     })
 });
 
+router.get("/admin/users/create", (req, res) => {
+    res.render("admin/users/create");
+});
 
+router.post("/users/create", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    User.findOne({
+        where: {
+            email: email
+        }
+    }).then((user) => {
+
+        if (user == undefined) {
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+
+            User.create({
+                    email: email,
+                    password: hash,
+                }).then(() => {
+                    res.redirect("/");
+                })
+                .catch((err) => {
+                    res.redirect("/");
+                    console.error(err);
+                });
+        } else {
+            res.redirect("/admin/users/create");
+        }
+    });
+});
 
 module.exports = router;
